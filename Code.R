84026d52bb81584e7c50b7b39d010ac3db134d22
xblueberry <u0117439@kuleuven.be>
2018-04-30 20:33:49 +0200

    update

--- Code.R
+++ Code.R
@@ -3,8 +3,12 @@ rm(list = ls())
 library(data.table)
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
+library(PerformanceAnalytics)
 library(VIM)
 library(DMwR)
+library(ROCR)
+
 
 df <- fread("C:/Users/u0117439/Desktop/MSc Statistics/Advanced Analytics in Bussiness/Assignments/Assignment 2/hmeq.csv",
               sep=",",header = T)
@@ -41,6 +45,9 @@ ggplot(dfmelt2, aes(x=factor(BAD), y=value,fill=variable))+
   theme(axis.text.x=element_text(angle=-90, vjust=0.4,hjust=1))
 
 
+#Correlation of continuous variables
+chart.Correlation(df[,c("LOAN","MORTDUE","VALUE","YOJ","CLAGE","DEBTINC")], histogram=TRUE, pch=19)
+
 
 ############################# Missing data ########################
 
@@ -48,7 +55,7 @@ ggplot(dfmelt2, aes(x=factor(BAD), y=value,fill=variable))+
 
 aggr_plot <- aggr(df, col=c('navyblue','red'), sortVars=TRUE, numbers=TRUE, labels=names(df), cex.axis=.5, gap=2, ylab=c("Histogram of missing data","Pattern"))
 
-#Create new category "Unknown"
+#Create new category "Unknown" for REASON and JOB
 levels <- levels(df$REASON)
 levels[1] <- "Unknown"
 df$REASON <- factor(df$REASON, levels = levels)
@@ -61,33 +68,49 @@ df$JOB <- factor(df$JOB, levels = levels)
 df$JOB[is.na(df$JOB)] <- "Unknown"
 df[df$JOB=="",]$JOB<-"Unknown"
 
-# index
-set.seed(123); index = sample(1:nrow(df_clean), size = floor(nrow(df_clean)*0.80))
-df_tr = df_clean[index,]
-df_ts = df_clean[-index,]
+
+#Split dataset 
+#70-30 split
+set.seed(123); index = sample(1:nrow(df), size = floor(nrow(df)*0.70))
+df_tr = df[index,]
+df_ts = df[-index,]
+
 
 #knn imputation 
-df_clean<-knnImputation(df_tr, k = 10, scale = T, 
-                          meth = "weighAvg",distData = NULL)
-summary(df_clean[,c("BAD","LOAN","MORTDUE","VALUE","YOJ","DEROG",
+#df_clean<-knnImputation(df_tr, k = 10, scale = T, 
+#                          meth = "weighAvg",distData = NULL)
+#round
+#df_clean<-as.data.table(df_clean)
+#cols <- c("LOAN","MORTDUE","VALUE","DEROG","DELINQ","NINQ","CLNO")
+#df_clean[,(cols) := round(.SD,0), .SDcols=cols]
+#df_clean
+
+
+#median imputation of training set
+num_cols <- names(df_tr)[sapply(df_tr, is.numeric)]
+for(col in num_cols) {
+  set(df_tr, i = which(is.na(df_tr[[col]])), j = col, value = median(df_tr[[col]], na.rm=TRUE))
+}
+
+summary(df_tr[,c("BAD","LOAN","MORTDUE","VALUE","YOJ","DEROG",
                       "DELINQ","CLAGE","NINQ","CLNO","DEBTINC")])
 
-#round
-df_clean<-as.data.table(df_clean)
-cols <- c("LOAN","MORTDUE","VALUE","DEROG","DELINQ","NINQ","CLNO")
-df_clean[,(cols) := round(.SD,0), .SDcols=cols]
-df_clean
+#median imputation of test set (with training median)
+num_cols_test <- names(df_ts)[sapply(df_ts, is.numeric)]
+for(col in num_cols_test) {
+  set(df_ts, i = which(is.na(df_ts[[col]])), j = col, value = median(df_tr[[col]], na.rm=TRUE))
+}
+
+
+###################################### Outliers ###############################
+
 
 
 ###################################### Logistic regression ###############################
 
-
-
-
 fit<-glm(BAD~.,data = df_tr,family="binomial")
 summary(fit)
 
-library(ROCR)
 
 #ROC curve
 p <- predict(fit, newdata=df_ts, type="response")
@@ -100,4 +123,3 @@ auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
 
-#try
