e534ce79eb24bf05b8126619ca42b2923ff7c84e
anjabelcijan <anja.belcijan@gmail.com>
2018-05-04 16:55:18 +0200

    n
    new

--- Modeling.R
+++ Modeling.R
@@ -10,7 +10,8 @@ library(rattle)
 library(rpart)
 library(rpart.plot)
 library(C50)
-
+library(caret)
+library(lattice)
 #read pre-processed training and test datasets
 df_tr <- fread("/Users/anjabelcijan/Desktop/df_tr.csv",sep=";",header = T)
 df_ts <- fread("/Users/anjabelcijan/Desktop/df_ts.csv",sep=";",header = T)
@@ -42,9 +43,9 @@ auc
 #basic tree#
 treeimb <- rpart(BAD ~ .,data = df_tr)
 treeimb
-
 pred.treeimb <- predict(treeimb, newdata = df_ts)
 head(pred.treeimb)
+
 # plots #
 prp(treeimb)
 fancyRpartPlot(treeimb)
@@ -52,10 +53,22 @@ printcp(treeimb)
 plotcp(treeimb)
 summary(treeimb)
 
+################Using C5.0################
+
+#make bad a factor#
+
+df_tr$BAD <-as.factor(df_tr$BAD)
+df_ts$BAD <-as.factor(df_ts$BAD)
+
 #basic c5.0 #
-tree_mod <- C5.0(df_tr[,-1], as.factor(df_tr$BAD),controL=C5.0Control(winnow = TRUE))
-summary(tree_mod)
-plot(tree_mod)
+tree_1 <- C5.0(df_tr[,-1], df_tr$BAD)
+summary(tree_1)
+plot(tree_1)
+
+tree_1_pred  <- predict(tree_1, df_ts)
+tree_1_probs <- predict(tree_1, df_ts, type ="prob")
+postResample(tree_1_probs, df_ts$BAD)
+
 #prediction not working#
 predmod<- predict(tree_mod , df_ts[,-1], type="prob")
 str(predmod)
@@ -72,7 +85,7 @@ summary(tree_boost)
 plot(tree_boost)
 
 # dealing with class imbalance problem #
-table(df_tr$BAD )
+table(df_tr$BAD)
 #undersample
 df_under <- ovun.sample(BAD ~ ., data = df_tr, method = "under",
                         p = 0.5, seed = 1)$data
