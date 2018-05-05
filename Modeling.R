33f5fee1443fa5cbe292e166c6e24cf08d9d1b01
anjabelcijan <anja.belcijan@gmail.com>
2018-05-05 09:27:12 +0200

    added dt metrics

--- Modeling.R
+++ Modeling.R
@@ -11,7 +11,7 @@ library(rpart)
 library(rpart.plot)
 library(C50)
 library(caret)
-library(lattice)
+INlibrary(lattice)
 #read pre-processed training and test datasets
 df_tr <- fread("/Users/anjabelcijan/Desktop/df_tr.csv",sep=";",header = T)
 df_ts <- fread("/Users/anjabelcijan/Desktop/df_ts.csv",sep=";",header = T)
@@ -43,8 +43,13 @@ auc
 #basic tree#
 treeimb <- rpart(BAD ~ .,data = df_tr)
 treeimb
-pred.treeimb <- predict(treeimb, newdata = df_ts)
-head(pred.treeimb)
+prob.treeimb <- predict(treeimb, newdata=df_ts , type="prob")
+pred.treeimb <- prediction(prob.treeimb[,'1'], df_ts$BAD)
+perf.treeimb <- performance(pred.treeimb, measure="tpr", x.measure="fpr")
+plot(perf.treeimb)
+auc.treeimb <- performance(pred.treeimb, measure="auc")
+auc.treeimb <- auc.treeimb@y.values[[1]]
+auc.treeimb
 
 # plots #
 prp(treeimb)
@@ -61,28 +66,73 @@ df_tr$BAD <-as.factor(df_tr$BAD)
 df_ts$BAD <-as.factor(df_ts$BAD)
 
 #basic c5.0 #
-tree_1 <- C5.0(df_tr[,-1], df_tr$BAD)
+tree_1 <- C5.0(df_tr$BAD~., data=df_tr)
 summary(tree_1)
 plot(tree_1)
 
-tree_1_pred  <- predict(tree_1, df_ts)
-tree_1_probs <- predict(tree_1, df_ts, type ="prob")
-postResample(tree_1_probs, df_ts$BAD)
+# auc=0.87
+# acc=
 
-#prediction not working#
-predmod<- predict(tree_mod , df_ts[,-1], type="prob")
-str(predmod)
-pred <- prediction(predictions = predmod, labels = df_ts$BAD)
-perf <- performance(pred, measure = "tpr", x.measure = "fpr")
+# c5.0 with winnowing 
+tree_2 <- C5.0(df_tr$BAD ~ ., data = df_tr, control = C5.0Control(winnow = TRUE))
+summary(tree_2)
+plot(tree_2)
+
+# auc=0.873
+
+# c5.0 with pruning control
+tree_3 <-C5.0(df_tr$BAD ~ ., data = df_tr, control = C5.0Control(CF = .01))
+summary(tree_3)
+plot(tree_3)
+
+# auc=0.86
+
+# c5.0 with min cases control
+tree_4 <-C5.0(df_tr$BAD ~ ., data = df_tr, control = C5.0Control(minCases = 150))
+summary(tree_4)
+plot(tree_4)
+
+# auc=0.84
 
 # c5.0 with rules #
-tree_mod1 <- C5.0(x=df_tr[,-1], y=as.factor(df_tr$BAD), rules=TRUE)
-tree_mod1
+tree_5 <- C5.0(x=df_tr[,-1], y=as.factor(df_tr$BAD), rules=TRUE)
+summary(tree_5)
+
+# auc=0.83
 
 # boosted tree # 
-tree_boost <- C5.0(x = df_tr[,-1], y = as.factor(df_tr$BAD), trials = 5)
+tree_boost <- C5.0(x = df_tr[,-1], y = as.factor(df_tr$BAD), trials = 30)
 summary(tree_boost)
 plot(tree_boost)
+
+#auc calculation
+
+prob.tree1 <- predict(tree_5, newdata=df_ts , type="prob")
+pred.tree1 <- prediction(prob.tree1[,'1'], df_ts$BAD)
+perf.tree1 <- performance(pred.tree1, measure="tpr", x.measure="fpr")
+plot(perf.tree1)
+auc.tree1 <- performance(pred.tree1, measure="auc")
+auc.tree1 <- auc.tree1@y.values[[1]]
+auc.tree1
+
+#accuracy
+acc.tree1 <- performance(pred.tree1, measure="acc")
+acc.tree1 <- max(acc.tree1@y.values[[1]])
+acc.tree1
+
+# error rate
+err.tree1 <- performance(pred.tree1, measure="err")
+err.tree1 <- ave(err.tree1@y.values[[1]])
+err.tree1
+
+
+prob.tree1 <- predict(tree_5, newdata=df_ts , type="prob")
+pred.tree1 <- prediction(prob.tree1[,'1'], df_ts$BAD)
+perf.tree1 <- performance(pred.tree1, measure="tpr", x.measure="fpr")
+plot(perf.tree1)
+auc.tree1 <- performance(pred.tree1, measure="auc")
+auc.tree1 <- auc.tree1@y.values[[1]]
+auc.tree1
 
 # dealing with class imbalance problem #
 table(df_tr$BAD)
